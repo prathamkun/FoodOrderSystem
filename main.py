661973f35b4c0c3327e2a3b219dfbc8e471d889a
prathamkun <216220075+prathamkun@users.noreply.github.com>
2026-04-01 23:11:48 +0530

    changes

--- main.py
+++ main.py
@@ -9,3 +9,11 @@ conn = mysql.connector.connect(
 cursor = conn.cursor()
 
 print("Connected to MySQL ✅")
+
+def get_menu():
+    cursor.execute("SELECT * FROM menu")
+    return cursor.fetchall()
+    menu = get_menu()
+
+    for item in menu:
+        print(item)
